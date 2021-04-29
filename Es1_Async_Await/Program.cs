8c27f0bdef6ea67fbbe23b96c53afcfa384ba750
Filippo <Dell@DESKTOP-2751OC0>
2021-04-29 10:51:46 +0200

    Aggiungere i file di progetto.

---
+++ Es1_Async_Await/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Es1_Async_Await
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[100];
+            RiempiArray(array);
+
+            Console.WriteLine("Inserisci un numero da cercare:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(RicercaNumeroAsync(n, array).Result);
+
+            Console.WriteLine("Inserisci un numero da cercare:");
+            int n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(RicercaNumeroAsync(n2, array).Result);
+        }
+        private static async Task<bool> RicercaNumeroAsync(int n, int[] array)
+        {
+            bool trovato = false;
+
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    if (n == array[i])
+                    {
+                        trovato = true;
+                    }
+                }
+            });
+            return trovato;
+        }
+        private static void RiempiArray(int[] array)
+        {
+            Random r = new Random();
+            for (int n = 0; n < 100; n++)
+            {
+                array[n] = r.Next(0, 100);
+            }
+        }
+
+    }
+}
